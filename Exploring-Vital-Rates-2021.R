53624b17803fce1bde2fcbab1562de19b132fddd
slaubrie <aubriejames12@gmail.com>
2022-08-07 14:42:13 -0400

    split up the 2021 and 2022 analysis and added model comparison

--- Exploring-Vital-Rates-2021.R
+++ Exploring-Vital-Rates-2021.R
@@ -7,6 +7,8 @@ require(pscl)
 require(glmmTMB)
 require(tidyr)
 require(DHARMa)
+require(ggplot2)
+require(AICcmodavg)
 
 ############## 2021 data #############
 
@@ -128,6 +130,7 @@ emmip(countfit_leg,~initial|name, type='response',CI=T)
 est<-emmeans(countfit_leg, ~initial|name, type='response')
 pairs(est)
 
+
 ############# log legacy response: do analysis for total weight by species ############# 
 
 # weights
@@ -235,131 +238,49 @@ emmip(countfit_intxn,~physical_barrier|name|initial, type='response',CI=T)
 est<-emmeans(countfit_intxn, ~physical_barrier|name|initial, type='response')
 pairs(est)
 
-############## 2022 data ##############
+### adding 
+zerofit_add<-glmmTMB(presence~name*physical_barrier+name*initial+(1 | block), family=binomial, data=countdat)
 
-# read csv
-dat<-read.csv('nplants_data_2022.csv', header=T)
-dat1<-dat[which(dat$seeding_trt==1),]
-dat1$physical_barrier<-as.factor(dat1$physical_barrier)
-dat1$block<-as.factor(dat1$block)
-dat2<-dat1[,c(1,3:4,7:9,13)]
-dat22<-as.data.frame(dat2 %>% pivot_longer(c(ntrcy_germ, ngoro_germ, ntror_germ)))
-dat22$attempts<-rep(15, nrow(dat22))
-dat22$fails<-dat22$attempts-dat22$value
-dat22$presence<-ifelse(countdat$value==0, 0, 1)
-dat22$posicounts<-as.numeric(ifelse(dat22$value==0, "NA", dat22$value))
+countfit_add<-countfit_intxn<-glmmTMB(posicounts~name*physical_barrier+name*initial+(1 | block), family=truncated_nbinom2(), data=countdat)
 
-############# treatment response: do analysis for germination by species ############# 
-# I think this doens't work because it's mostly zeros.
-# fit<-glmmTMB(value~name*current_plot_type+(1| block), ziformula=~., family=poisson(), data=dat22)
-#  sim<-simulateResiduals(fit)
-#  plot(sim)
-#  testDispersion(sim)
-#  testZeroInflation(sim)
-#  
-#  summary(fit)
-#  emmip(fit, ~current_plot_type|name, type='response', CI=T)
-#  est<-emmeans(fit,~current_plot_type|name, type='response')
-#  pairs(est)
+### per capita biomass 
 
-### zeros and ones 
-zerofit<-glmmTMB(presence~name*current_plot_type+(1 | block), family=binomial, data=dat22)
-emmip(zerofit,~current_plot_type|name, type='response',CI=T)
-est<-emmeans(zerofit, ~current_plot_type|name, type='response')
-pairs(est)
+# model - intxn
+pcwtmod_intxn<-lmer(log_wt~name*physical_barrier*initial+(1|block), data=pcwtdat)
 
-### abundance with a truncated negbinom 
-countfit<-glmmTMB(posicounts~name*current_plot_type+(1 | block), family=truncated_nbinom2(), data=dat22)
-emmip(countfit,~current_plot_type|name, type='response',CI=T)
-est<-emmeans(countfit, ~current_plot_type|name, type='response')
-pairs(est)
- 
-######### What about the log legacy - initial treatment for 2022 #######
-
-fit_leg<-glmmTMB(value~name*initial+(1 | block), ziformula=~., family=poisson(), data=dat22)
-sim<-simulateResiduals(fit_leg)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
-
-summary(fit_leg)
-emmip(fit_leg, ~initial|name, type='response', CI=T)
-est<-emmeans(fit_leg,~initial|name, type='response')
-pairs(est)
-
-# split up occurrence and abundance
-### zeros and ones 
-zerofit_leg<-glmmTMB(presence~name*initial+(1 | block), family=binomial, data=dat22)
-emmip(zerofit_leg,~initial|name, type='response',CI=T)
-est<-emmeans(zerofit_leg, ~initial|name, type='response')
-pairs(est)
-
-### abundance with a truncated negbinom 
-countfit_leg<-glmmTMB(posicounts~name*initial+(1 | block), family=truncated_nbinom2(), data=dat22)
-emmip(countfit_leg,~initial|name, type='response',CI=T)
-est<-emmeans(countfit_leg, ~initial|name, type='response')
-pairs(est)
-
-######### What about the physical barrier - initial treatment for 2022 #######
-
-fit_phys<-glmmTMB(value~name*physical_barrier+(1 | block), ziformula=~., family=nbinom2(), data=dat22)
-sim<-simulateResiduals(fit_phys)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
-
-summary(fit_phys)
-emmip(fit_phys, ~physical_barrier|name, type='response', CI=T)
-est<-emmeans(fit_phys,~physical_barrier|name, type='response')
-pairs(est)
-
-# split up occurrence and abundance
-### zeros and ones 
-zerofit_phys<-glmmTMB(presence~name*physical_barrier+(1 | block), family=binomial, data=dat22)
-
-sim<-simulateResiduals(zerofit_phys)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
-
-emmip(zerofit_phys,~physical_barrier|name, type='response',CI=T)
-est<-emmeans(zerofit_phys, ~physical_barrier|name, type='response')
-pairs(est)
-
-### abundance with a truncated negbinom 
-countfit_phys<-glmmTMB(posicounts~name*physical_barrier+(1 | block), family=truncated_nbinom2(), data=dat22)
-
-sim<-simulateResiduals(countfit_phys)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
-
-emmip(countfit_phys,~physical_barrier|name, type='response',CI=T)
-est<-emmeans(countfit_phys, ~physical_barrier|name, type='response')
-pairs(est)
+# model - no intxn
+pcwtmod_add<-lmer(log_wt~name*physical_barrier+name*initial+(1|block), data=pcwtdat)
 
 
-#### What about both physical and legacy? #### 
-### zeros and ones 
-zerofit_intxn<-glmmTMB(presence~name*physical_barrier*initial+(1 | block), family=binomial, data=dat22)
+#### 2021 model comparison for counts #### 
+## zeros 
+zero_candmods<-list("Plot type"=zerofit, 
+                    "Physical barrier"=zerofit_phys,
+                    "Nutrient island"=zerofit_leg,
+                    "Physical Barrier + Nutrient Island"=zerofit_add,
+                    "Physical Barrier x Nutrient Island"=zerofit_intxn)
+aictab(zero_candmods)
 
-sim<-simulateResiduals(zerofit_intxn)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
+# counts
+count_candmods<-list("Plot type"=countfit, 
+                     "Physical barrier"=countfit_phys,
+                     "Nutrient island"=countfit_leg,
+                     "Physical Barrier + Nutrient Island"=countfit_add,
+                     "Physical Barrier x Nutrient Island"=countfit_intxn)
+aictab(count_candmods)
 
-emmip(zerofit_intxn,initial~physical_barrier|name, type='response',CI=T)
-est<-emmeans(zerofit_intxn, ~physical_barrier|name|initial, type='response')
-pairs(est)
+#### 2021 model comparison for per capita biomass #### 
+pcwt_candmods<-list("Plot type"=pcwtmod, 
+                    "Physical barrier"=pcwtmod_phys,
+                    "Nutrient island"=pcwtmod_leg,
+                    "Physical Barrier + Nutrient Island" = pcwtmod_add,
+                    "Physical Barrier x Nutrient Island"=pcwtmod_intxn)
+aictab(pcwt_candmods)
 
-### abundance with a truncated negbinom 
-countfit_intxn<-glmmTMB(posicounts~name*physical_barrier*initial+(1 | block), family=truncated_nbinom2(), data=dat22)
-
-sim<-simulateResiduals(countfit_intxn)
-plot(sim)
-testDispersion(sim)
-testZeroInflation(sim)
-
-emmip(countfit_intxn,~physical_barrier|name|initial, type='response',CI=T)
-est<-emmeans(countfit_intxn, ~physical_barrier|name|initial, type='response')
-pairs(est)
+#### plots for later 
+# +
+#   theme_bw()+
+#   theme(strip.text.x = element_text(size=0),
+#         strip.background = element_blank())+
+#   xlab("Log legacy")+
+#   ylab("Abundance")
